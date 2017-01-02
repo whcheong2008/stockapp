4eafc2411d0099a3e0bad8e9fbcf776f4c637ec7
geraldinetay27 <geraldinetay27@gmail.com>
2017-01-02 23:05:30 +0800

    Fixed Alerts Bug and Skin-Config.html error
    - Fixed Alerts Dropdown not working.
    - Fixed calls to missing skin-config.html

--- public/javascripts/inspinia.js
+++ public/javascripts/inspinia.js
@@ -98,11 +98,11 @@ $(document).ready(function () {
 
     // Append config box / Only for demo purpose
     // Uncomment on server mode to enable XHR calls
-    $.get("skin-config.html", function (data) {
+/*    $.get("skin-config.html", function (data) {
         if (!$('body').hasClass('no-skin-config'))
             $('body').append(data);
     });
-
+*/
     // Minimalize menu
     $('.navbar-minimalize').on('click', function () {
         $("body").toggleClass("mini-navbar");
